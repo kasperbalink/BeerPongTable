e7b2261f3f0411da4f79dd47c1c66a2dfa3b8489
Kasper Balink <kasperbalink@hotmail.com>
2015-10-13 13:02:54 +0200

    nieuwe aansluiting

--- BeerpongTable/BeerpongTable/BeerpongTable.ino
+++ BeerpongTable/BeerpongTable/BeerpongTable.ino
@@ -1,25 +1,42 @@
-/* 
+/*
 *	Title: Beerpong Table
 *	Author: Kasper Balink
 *	Date: 08-July-2015
-*	
+*
 */
 
 //this code will display the values of ledData across a 4x4 led matrix
 #include "elapsedMillis.h"
 #include "visualisation.h"
 //pin connections- the #define tag will replace all instances of "latchPin" in your code with A1 (and so on)
-#define latchPin A1
-#define clockPin A0
-#define dataPin A2
+
+
+#define HC259_A_PIN A0
+#define HC259_B_PIN A1
+#define HC259_C_PIN A2
+
+#define latchPin  A0
+#define clockPin A1
+#define dataPin  A2
+
+#define latchPinB  A3
+#define clockPinB A4
+#define dataPinB  A5
+
+boolean HC259_address[3] = { 0, 0, 0 };
+boolean HC259_data[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
 visualisation visual;
 
 //storage for led states, 4 bytes
-byte r[] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+byte r[] = { 128, 64, 32, 16, 8, 4, 2, 1 };
+//byte r[] = { 1, 2, 4, 8, 16, 32, 64, 128};
 byte c[] = { 1, 2, 4, 8, 16, 32, 64, 128 };
 void setup()
 {
+	pinMode(HC259_A_PIN, OUTPUT);
+	pinMode(HC259_B_PIN, OUTPUT);
+	pinMode(HC259_C_PIN, OUTPUT);
 	//set pins as output
 	pinMode(latchPin, OUTPUT);
 	pinMode(clockPin, OUTPUT);
@@ -35,6 +52,94 @@ void writeLed(int column, int row)
 	//	shiftOut(dataPin, clockPin, LSBFIRST, r[column]);
 	shiftOut(dataPin, clockPin, LSBFIRST, r[row]);
 	shiftOut(dataPin, clockPin, LSBFIRST, c[column]);
+	//set latch pin high- this sends data to outputs so the LEDs will light up
+	digitalWrite(latchPin, HIGH);
+}
+
+void writeLed2(int row, int interval)
+{
+	elapsedMillis tempTimer;
+	tempTimer = 0;
+	while (tempTimer < interval)
+	{
+		//	byte dataToSend = ((c[column] | 15 & ~r[row]));
+		// setlatch pin low so the LEDs don't change while sending in bits
+		digitalWrite(latchPin, LOW);
+		// shift out the bits of dataToSend to the 74HC595
+		if (row < 8)
+		{
+			shiftOut(dataPin, clockPin, LSBFIRST, 0);
+			shiftOut(dataPin, clockPin, LSBFIRST, 0);
+			shiftOut(dataPin, clockPin, LSBFIRST, r[row]);
+		}
+		else if (row < 16)
+		{
+			shiftOut(dataPin, clockPin, LSBFIRST, 0);
+			shiftOut(dataPin, clockPin, LSBFIRST, r[row - 8]);
+			shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		}
+		else if (row < 24)
+		{
+			shiftOut(dataPin, clockPin, LSBFIRST, r[row - 16]);
+			shiftOut(dataPin, clockPin, LSBFIRST, 0);
+			shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		}
+		//set latch pin high- this sends data to outputs so the LEDs will light up
+		digitalWrite(latchPin, HIGH);
+	}
+}
+
+void allOff(int interval)
+{
+	digitalWrite(latchPin, LOW);
+	elapsedMillis tempTimer;
+	tempTimer = 0;
+	while (tempTimer < interval)
+	{
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+	}
+	digitalWrite(latchPin, HIGH);
+}
+void allOn(int interval)
+{
+	digitalWrite(latchPin, LOW);
+	elapsedMillis tempTimer;
+	tempTimer = 0;
+	while (tempTimer < interval)
+	{
+		for (int i = 0; i < 19; i++)
+		{
+			row(i);
+		}
+	}
+	digitalWrite(latchPin, HIGH);
+}
+void row(int x)
+{
+	//	byte dataToSend = ((c[column] | 15 & ~r[row]));
+	// setlatch pin low so the LEDs don't change while sending in bits
+	digitalWrite(latchPin, LOW);
+	// shift out the bits of dataToSend to the 74HC595
+	if (x < 8)
+	{
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		shiftOut(dataPin, clockPin, LSBFIRST, r[x]);
+	}
+	else if (x < 16)
+	{
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		shiftOut(dataPin, clockPin, LSBFIRST, r[x - 8]);
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+	}
+	else if (x < 24)
+	{
+		shiftOut(dataPin, clockPin, LSBFIRST, r[x - 16]);
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+		shiftOut(dataPin, clockPin, LSBFIRST, 0);
+	}
 	//set latch pin high- this sends data to outputs so the LEDs will light up
 	digitalWrite(latchPin, HIGH);
 }
@@ -183,54 +288,194 @@ void squareOutside(int interval)
 		column(3);*/
 	}
 }
-void loop()
+void line(int size, int interval)
 {
-	/*visual.drawLetter('a', 0, 0, 2000);
-	visual.drawLetter('b', 0, 0, 2000);
-	visual.drawLetter('c', 0, 0, 2000);
-	visual.drawLetter('d', 0, 0, 2000);
-	visual.drawLetter('e', 0, 0, 2000);*/
-	for (int i = 0; i < 5; i++)
-	{
-		for (int x = -7; x <= 7; x++)
-		{
-			visual.arrow(1, x, 0, 50);
-		}
-	}
+	int min = 0;
+	int max = 0;
 
-	for (int i = 0; i < 5; i++)
+	while (max < size)
 	{
-		for (int x = 7; x >= -7; x--)
+		elapsedMillis tempTimer;
+		tempTimer = 0;
+		while (tempTimer < interval)
 		{
-			visual.arrow(3, x, 0, 50);
-		}
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		visual.upToDown(50, 0);
-		visual.downToUp(50, 1);
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		visual.leftToRight(50, 0);
-		visual.rightToLeft(50, 1);
-	}
-	for (int i = 0; i < 6; i++)
-	{
-		for (int x = 0; x < 4; x++)
-		{
-			elapsedMillis tempTimer;
-			tempTimer = 0;
-			while (tempTimer < 400)
+			for (int i = min; i < max; i++)
 			{
-				visual.bracket(1, x - 4, 0);
-				visual.bracket(1, x, 0);
-				visual.bracket(1, x + 4, 0);
+				row(i);
 			}
 		}
+		max++;
+	}
+	while (min <= 19)
+	{
+		elapsedMillis tempTimer;
+		tempTimer = 0;
+		while (tempTimer < interval)
+		{
+			for (int i = min; i < max; i++)
+			{
+				row(i);
+			}
+		}
+		min++;
+		max++;
+	}
+	while (max >= 0)
+	{
+		elapsedMillis tempTimer;
+		tempTimer = 0;
+		while (tempTimer < interval)
+		{
+			for (int i = min; i < max; i++)
+			{
+				row(i);
+			}
+		}
+		if (min != 0)
+		{
+			min--;
+		}
+		max--;
+	}
+}
+void loop()
+{
+	//digitalWrite(latchPin, LOW);
+	//	// shift out the bits of dataToSend to the 74HC595
+	//	shiftOut(dataPin, clockPin, LSBFIRST, ~255);
+	//	shiftOut(dataPin, clockPin, LSBFIRST, ~255);
+
+	//	shiftOut(dataPin, clockPin, LSBFIRST, 255);
+	//	shiftOut(dataPin, clockPin, LSBFIRST, 255);
+	//	shiftOut(dataPin, clockPin, LSBFIRST, 255);
+	//	digitalWrite(latchPin, HIGH);
+	elapsedMillis tempTimer;
+	tempTimer = 0;
+	while (tempTimer < 500)
+	{
+		for (int i = 128; i > 1; i = i / 2)
+		{
+			digitalWrite(latchPin, LOW);
+			// shift out the bits of dataToSend to the 74HC595
+			shiftOut(dataPin, clockPin, LSBFIRST, ~255);
+			shiftOut(dataPin, clockPin, LSBFIRST, ~255);
+
+			shiftOut(dataPin, clockPin, LSBFIRST, i);
+			shiftOut(dataPin, clockPin, LSBFIRST, i);
+			shiftOut(dataPin, clockPin, LSBFIRST, i);
+			digitalWrite(latchPin, HIGH);
+			delay(100);
+		}
 	}
 
+	tempTimer = 0;
+	while (tempTimer < 500)
+	{
+		for (int i = 128; i > 1; i = i / 2)
+		{
+			digitalWrite(latchPin, LOW);
+			// shift out the bits of dataToSend to the 74HC595
+			shiftOut(dataPin, clockPin, LSBFIRST, ~i);
+			shiftOut(dataPin, clockPin, LSBFIRST, ~i);
+
+			shiftOut(dataPin, clockPin, LSBFIRST, 255);
+			shiftOut(dataPin, clockPin, LSBFIRST, 255);
+			shiftOut(dataPin, clockPin, LSBFIRST, 255);
+			digitalWrite(latchPin, HIGH);
+			delay(100);
+		}
+	}
+
+	
+
+	// shift out the bits of dataToSend to the 74HC595
+	//shiftOut(dataPinB, clockPinB, LSBFIRST, 256);
+	//digitalWrite(latchPinB, HIGH);
+
+	//HC259_send_data();
 }
 
+void HC259_send_data()
+{
+	for (int i = 0; i <= 7; i++)
+	{
+		HC259_set_address(i);
+	//	digitalWrite(HC259_data_pin, HC259_data[i]);
+	}
+}
+
+void HC259_set_address(int latch)
+{
+	switch (latch)
+	{
+	case 0:
+		HC259_address[0] = 0;
+		HC259_address[1] = 0;
+		HC259_address[2] = 0;
+		digitalWrite(HC259_A_PIN, 0);
+		digitalWrite(HC259_B_PIN, 0);
+		digitalWrite(HC259_C_PIN, 0);
+		break;
+	case 1:
+		HC259_address[0] = 0;
+		HC259_address[1] = 0;
+		HC259_address[2] = 1;
+		digitalWrite(HC259_A_PIN, 0);
+		digitalWrite(HC259_B_PIN, 0);
+		digitalWrite(HC259_C_PIN, 1);
+		break;
+	case 2:
+		HC259_address[0] = 0;
+		HC259_address[1] = 1;
+		HC259_address[2] = 0;
+		digitalWrite(HC259_A_PIN, 0);
+		digitalWrite(HC259_B_PIN, 1);
+		digitalWrite(HC259_C_PIN, 0);
+		break;
+	case 3:
+		HC259_address[0] = 0;
+		HC259_address[1] = 1;
+		HC259_address[2] = 1;
+		digitalWrite(HC259_A_PIN, 0);
+		digitalWrite(HC259_B_PIN, 1);
+		digitalWrite(HC259_C_PIN, 1);
+		break;
+	case 4:
+		HC259_address[0] = 1;
+		HC259_address[1] = 0;
+		HC259_address[2] = 0;
+		digitalWrite(HC259_A_PIN, 1);
+		digitalWrite(HC259_B_PIN, 0);
+		digitalWrite(HC259_C_PIN, 0);
+		break;
+	case 5:
+		HC259_address[0] = 1;
+		HC259_address[1] = 0;
+		HC259_address[2] = 1;
+		digitalWrite(HC259_A_PIN, 1);
+		digitalWrite(HC259_B_PIN, 0);
+		digitalWrite(HC259_C_PIN, 1);
+		break;
+	case 6:
+		HC259_address[0] = 1;
+		HC259_address[1] = 1;
+		HC259_address[2] = 0;
+		digitalWrite(HC259_A_PIN, 1);
+		digitalWrite(HC259_B_PIN, 1);
+		digitalWrite(HC259_C_PIN, 0);
+		break;
+	case 7:
+		HC259_address[0] = 1;
+		HC259_address[1] = 1;
+		HC259_address[2] = 1;
+		digitalWrite(HC259_A_PIN, 1);
+		digitalWrite(HC259_B_PIN, 1);
+		digitalWrite(HC259_C_PIN, 1);
+		break;
+	default:
+		break;
+		//nothing;
+	}
+}
 
 
